71995e8ff2cc74314f651987ddbf6d73ce65a761
cywin1018 <cywin0821@gmail.com>
2023-09-21 22:33:26 +0900

    start 09/21

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvzrW3u8ynxz0B58Tv4HUoJ0cAIu9TWuY",
+  authDomain: "nwitter-reloaded-9cd2d.firebaseapp.com",
+  projectId: "nwitter-reloaded-9cd2d",
+  storageBucket: "nwitter-reloaded-9cd2d.appspot.com",
+  messagingSenderId: "544010856717",
+  appId: "1:544010856717:web:654b55fddd5ce085556844"
+};
+
+
+const app = initializeApp(firebaseConfig);
