9492da08ee3202211bfe9f9a1443180abc181bd4
cywin1018 <cywin0821@gmail.com>
2023-11-07 11:37:01 +0900

    update 1107

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,15 +1,14 @@
-import { auth } from "../firebase";
 import styled from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
 
-const LogOut = styled.button`
-  width: 100px;
-  height: 20px;
-`;
+const Wrapper = styled.div``;
 
 export default function Home() {
-  const logOut = () => {
-    auth.signOut();
-    console.log("success log out maybe");
-  };
-  return <LogOut onClick={logOut}>Log-Out</LogOut>;
+  return (
+    <>
+      <Wrapper>
+        <PostTweetForm />
+      </Wrapper>
+    </>
+  );
 }
